65d49fec85584fffcfac69e4eff9b07cd236eb57
Alessio <alewatto@live.it>
2024-12-28 15:21:37 +0100

    .

---
+++ dev/v3/_next/static/chunks/pages/skeleton-9e5fc82ca8f87415.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[510],{9768:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/skeleton",function(){return u(2846)}])}},function(n){n.O(0,[922,846,774,888,179],function(){return n(n.s=9768)}),_N_E=n.O()}]);
